c3d5085691b4f636c1abb2851555f1622b2d5987
Horacio Gomez <horaciogomez.1993@gmail.com>
2016-08-18 20:48:41 -0300

    taller terminado

--- taller-syscalls/src/Solucion ej2/justiciero.c
+++ taller-syscalls/src/Solucion ej2/justiciero.c
@@ -8,8 +8,6 @@
 #include <errno.h>
 #include <syscall.h>
 
-using namespace std;
-
 int main(int argc, char* argv[]) {
 	int status;
 	pid_t child;
@@ -24,8 +22,9 @@ int main(int argc, char* argv[]) {
 	if (child == -1) { perror("ERROR fork"); return 1; }
 	if (child == 0) {
 		if (ptrace(PTRACE_TRACEME, 0, NULL, NULL)) {
-			perror("ERROR child ptrace(PTRACE TRACEME, ...)"); exit(1);
-		}		
+			perror("ERROR child ptrace(PTRACE TRACEME, ...)"); 
+			exit(1);
+		}
 		/* Solo se ejecuta en el hijo */
 		execvp(argv[1], argv+1);
 		/* Si vuelve de exec() hubo un error */
@@ -34,16 +33,20 @@ int main(int argc, char* argv[]) {
 		/* Solo se ejecuta en el padre */
 		while(1) {
 			if (wait(&status) < 0) { perror("waitpid"); break; }
+
 			if (WIFEXITED(status)) break; /* Proceso terminado */
-			ptrace(PTRACE_SYSCALL, child, NULL, NULL); /* Continua */		}
-			int sysno = ptrace(PTRACE_PEEKUSER, child, 4 * ORIG_EAX, NULL);
+
+			int sysno = ptrace(PTRACE_PEEKUSER, child, 8 * ORIG_RAX, NULL);
 			//printf("%d\n",sysno);
-			if(sysno == 62){
-				printf("%s\n","Se ha hecho justicia" );
+			if(sysno == SYS_kill){
 				ptrace(PTRACE_KILL, child, NULL, NULL);
-				
+				printf("%s\n","Se ha hecho justicia" );
+				exit(0);
 			}
+
+			ptrace(PTRACE_SYSCALL, child, NULL, NULL); /* Continua */
 		}
-	ptrace(PTRACE_DETACH, child, NULL, NULL);
+		ptrace(PTRACE_DETACH, child, NULL, NULL);
+	}
 	return 0;
 }
